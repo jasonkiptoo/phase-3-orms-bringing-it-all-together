577471a23929e31c7fdf14de2661f20bd55b6213
JasonKiptoo <kiptoojason@gmail.com>
2022-11-02 23:57:06 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,77 @@
 class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(id: nil, name:, breed:)
+      @id = id
+      @name = name
+      @breed = breed
+  end
+
+
+
+
+  
+  def self.create_table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+          id INTEGER PRIMARY KEY,
+          name text,
+          breed text
+      )
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+      sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+  def save
+      sql = <<-SQL
+      INSERT INTO dogs (id, name, breed) VALUES (?, ?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.id, self.name, self.breed)
+
+      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
+
+  def self.create(name:, breed:)
+      newDog = Dog.new(name: name, breed: breed)
+
+      newDog.save
+      newDog
+  end
+
+  def self.new_from_db(row)
+      self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.all
+      sql = <<-SQL
+      SELECT * FROM dogs
+      SQL
+      DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+  end
+
+
+  def self.find_by_name(name)
+      sql = <<-SQL
+      SELECT * FROM dogs WHERE name =? LIMIT 1
+      SQL
+      DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
+  end
+
+  def self.find(id)
+      sql = <<-SQL
+      SELECT * FROM dogs WHERE id=? LIMIT 1
+      SQL
+      DB[:conn].execute(sql, id).map {|row| self.new_from_db(row)}.first
+
+  end
+
 
 end
